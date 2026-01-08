334af98db603b16e664bba827467743eccbcb522
thaquidheen <thaquidheen@gmail.com>
2026-01-08 10:13:20 +0530

    ads

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
     <>
       <LeadPopup />
       <Hero />
-      <TrustedBy />
+      {/* <TrustedBy /> */}
       <AboutSection />
       <WhyUAE />
       <JurisdictionCards />
